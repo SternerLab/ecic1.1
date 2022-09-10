df0ae6badbe7c86eb49ed5c49d14aeb37a4d2532
Beckett Sterner <bsterne1@asu.edu>
2022-09-10 16:31:24 -0700

    Added more comments to code

--- R/ecic.R
+++ R/ecic.R
@@ -33,39 +33,50 @@ length.paleoTS = function(data){
 #'
 #' @export
 ECIC = function(models, data, alpha = c(0.01, 0.05, 0.1), N = 1000, ic = 'AIC', genBest = TRUE){
-  models = ecicModelList(models)
+  
+  #wrap user's inputted models into a list of ecicModel objects for modular handling
+  models = ecicModelList(models) #see ecic_model.R for ecicModel and ecicModelList function definitions
   p <- length(models)
 
   n <- length(data)
 
+  #calculate model scores using input data
   obs = lapply(models, function(x) IC(x, data, ic)) #observed
 
-
+  #extract parameter estimates and model scores
   if (!inherits(data, "paleoTS")) {
     params.obs = lapply(obs, function(x) x$parameters)
     scores.obs = sapply(obs, function(x) x$ic)
   } else {
-    n = length(data$nn) ## potential bug here since n is already set to length(data) before the if-else condition
+    n = length(data$nn) #potential bug here since n is already set to length(data) before the if-else condition
     params.obs = lapply(obs, function(x) x$parameters)
     scores.obs = sapply(obs, function(x) x$ic)
 
   }
-  weights.obs = AICweights(scores.obs)
+  #calculate Akaike weights from model scores
+  weights.obs = AICweights(scores.obs) #see ic_scores.R for function definition
+  
+  #identify the best scoring model
   best.ix = which.min(scores.obs)
   best = models[[best.ix]]
 
+  #identify the remaining alternative models
   alt.models = models[-best.ix]
 
-  dif.obs = scores.obs[best.ix]-min(scores.obs[-best.ix]) #take the score of the best model and subtract the best remaining score (where smaller is better)
-  ratio.obs = weights.obs[best.ix]/max(weights.obs[-best.ix]) #take the ratio of the observed best model's Akaike weight to the next best Akaike weight
+  #take the score of the best model and subtract the best remaining score (where smaller is better)
+  dif.obs = scores.obs[best.ix]-min(scores.obs[-best.ix]) 
+  
+  #take the ratio of the observed best model's Akaike weight to the next best Akaike weight
+  ratio.obs = weights.obs[best.ix]/max(weights.obs[-best.ix]) 
   names(dif.obs) = best$ID
 
-
+  #correct MLE estimates of alternative models to account for conditioning on not having the best observed score
   bc = lapply(alt.models, function(x)
     BiasCorrect(n, x, params.obs[[x$ID]],
-                models, N, ic, genBest))
+                models, N, ic, genBest)) #see bias_correct.R for function definition
 
-  icd = lapply(alt.models, function(x) ecicControl(n, x, bc[[x$ID]]$parameters, best, models, N, ic))
+  #use parametric bootstrapping to calculate score difference distributions
+  icd = lapply(alt.models, function(x) ecicControl(n, x, bc[[x$ID]]$parameters, best, models, N, ic)) #see ecic_control.R for function definition
 
 
   best.freq = sapply(icd, function(x) x$frequencies$frequencies[best.ix])
@@ -102,13 +113,14 @@ ECIC = function(models, data, alpha = c(0.01, 0.05, 0.1), N = 1000, ic = 'AIC', 
 
   names(ecic.thresholds) = alpha
 
+  #apply calculated decision thresholds to observed score difference
   decision.ecic = sapply(ecic.thresholds, function(thresh) ifelse(dif.obs > max(thresh),
                           best$ID, "No Decision")%>%unname)
 
   decision.ba = ifelse(ratio.obs > 2.7,
                        best$ID, "No Decision")%>%unname
 
-
+  #return primary and intermediate results 
   return(
     list(
       decisions = list(
@@ -127,6 +139,7 @@ ECIC = function(models, data, alpha = c(0.01, 0.05, 0.1), N = 1000, ic = 'AIC', 
   ) %>% structure(class = "ecic")
   }
 
+#placeholders for future custom methods for visualizing and debugging ECIC results
 #' @export
 summary.ecic = function(ecic){
   print("Summary Method")
