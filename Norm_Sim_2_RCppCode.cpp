9b7f7231d542190a0973b58803cdde38d9bfb45a
chengHStats <112587762+chengHStats@users.noreply.github.com>
2022-11-06 16:30:01 -0700

    Update Norm_Sim_2_RCppCode.cpp
    Improved code efficiency and added a function to simulate random normal draws

--- Norm_Sim_2_RCppCode.cpp
+++ Norm_Sim_2_RCppCode.cpp
@@ -2,43 +2,63 @@
 using namespace std;
 using namespace Rcpp;
 using namespace arma;
+#define pi 3.14159265358979
 // [[Rcpp::depends(RcppArmadillo)]]
 
 
-// work in progress code to simulate normal data
-/*
+// code to simulate normal data
 // [[Rcpp::export]]
-List normDatSim(arma::vec ns, List MLEList, int noDraws)
+List normDatSim(arma::vec ns,List MLEList,int noSimDraws)
 {
+    int nLen = MLEList.length(); // get the number of different n's
     List returnList(nLen); // the return list that indexes the sample sizes
-    int nLen = ns.length();
-    int mLen = MLEList.length();
-    for(int i=0;i<nLen;i++)
+    for(int i=0;i<nLen;++i) // index the sample sizes
     {
-
+        int tempN = ns(i);
+        List MLEsOfFixedN = MLEList[i];
+        int mLen=MLEsOfFixedN.length(); // get the number of different models
+        List returnListSub1(mLen); // part of return list that indexes the generating model
+        for(int j=0;j<mLen;++j)
+        {
+            arma::mat MLEsOfFixedNAndModel = MLEsOfFixedN[j];
+            int noDraws =  MLEsOfFixedNAndModel.n_cols;
+            List returnListSub2(noDraws); // part of return list that indexes the exact fits for each model
+            for(int k=0;k<noDraws;++k)
+            {
+                arma::vec tempMLEs = MLEsOfFixedNAndModel.col(k);
+                double tempMean = tempMLEs[0];
+                double tempSig = tempMLEs[1];
+                arma::mat tempNormDraws(tempN,noSimDraws,fill::randn);
+                tempNormDraws=tempNormDraws*tempSig+tempMean; // scale and translate values
+                returnListSub2[k] = tempNormDraws;
+            }
+            returnListSub1[j]=returnListSub2;
+        }
+        returnList[i]=returnListSub1;
+        Rcout << "Iteration " << i+1 << " Complete" << endl;
     }
+
+    return(returnList);
 }
-*/
 
 // compute the BIC for a normally distributed random variable
 // [[Rcpp::export]]
 double normBICComp(arma::vec ys, string model)
 {
-    double pi=3.14159265358979;
     double N = ys.n_elem;
     double BIC;
     if(model=="N(0,1)")
     {
         arma::vec sqrdDiffs = pow(ys-0,2);
         double LL = -N/2.0*log(2.0*pi)-N/2.0*log(1.0)-accu(sqrdDiffs)/(2.0*1.0);
-        BIC = -2*LL;
+        BIC = -2.0*LL;
     }
     else if(model=="N(mu,1)")
     {
         double MLEMean = mean(ys);
         arma::vec sqrdDiffs = pow(ys-MLEMean,2);
         double LL = -N/2.0*log(2.0*pi)-N/2.0*log(1.0)-accu(sqrdDiffs)/(2.0*1.0);
-        BIC = -2*LL + 1*log(N);
+        BIC = -2.0*LL + 1*log(N);
     }
     else // for the N(mu,sig) model
     {
@@ -47,9 +67,51 @@ double normBICComp(arma::vec ys, string model)
         double accuSqrdDiffs = accu(sqrdDiffs);
         double MLEVar = accuSqrdDiffs/N;
         double LL = -N/2.0*log(2.0*pi)-N/2.0*log(MLEVar)-accuSqrdDiffs/(2.0*MLEVar);
-        BIC = -2*LL + 2*log(N);
+        BIC = -2.0*LL + 2.0*log(N);
     }
     return(BIC);
+}
+
+// compute the BIC for a normally distributed random variable
+// [[Rcpp::export]]
+arma::vec normBICComp2(arma::mat tempYMat,string model,arma::vec fillVec, int noSimDraws)
+{
+    double N = tempYMat.n_rows;
+    if(model=="N(0,1)")
+    {
+        for(int d=0;d<noSimDraws;d++) // indexes data generated from a specific regression fit
+        {
+            arma::vec tempYs = tempYMat.col(d); // pull a particular simulated sample
+            arma::vec sqrdDiffs = pow(tempYs-0,2);
+            double LL = -N/2.0*log(2.0*pi)-N/2.0*log(1.0)-accu(sqrdDiffs)/(2.0*1.0);
+            fillVec[d] = -2.0*LL; // compute the BIC for this fit
+        }
+    }
+    else if(model=="N(mu,1)")
+    {
+        for(int d=0;d<noSimDraws;d++) // indexes data generated from a specific regression fit
+        {
+            arma::vec tempYs = tempYMat.col(d); // pull a particular simulated sample
+            double MLEMean = mean(tempYs);
+            arma::vec sqrdDiffs = pow(tempYs-MLEMean,2);
+            double LL = -N/2.0*log(2.0*pi)-N/2.0*log(1.0)-accu(sqrdDiffs)/(2.0*1.0);
+            fillVec[d] = -2.0*LL + 1.0*log(N); // compute the BIC for this fit
+        }
+    }
+    else // for the N(mu,sig) model
+    {
+        for(int d=0;d<noSimDraws;d++) // indexes data generated from a specific regression fit
+        {
+            arma::vec tempYs = tempYMat.col(d); // pull a particular simulated sample
+            double MLEMean = mean(tempYs);
+            arma::vec sqrdDiffs = pow(tempYs-MLEMean,2);
+            double accuSqrdDiffs = accu(sqrdDiffs);
+            double MLEVar = accuSqrdDiffs/N;
+            double LL = -N/2.0*log(2.0*pi)-N/2.0*log(MLEVar)-accuSqrdDiffs/(2.0*MLEVar);
+            fillVec[d] =  -2.0*LL + 2.0*log(N); // compute the BIC for this fit
+        }
+    }
+    return(fillVec);
 }
 
 // auxiliary function
@@ -103,11 +165,7 @@ List RcppICComps(List yList,Rcpp::StringVector models)
                     String curModel = models[m];
                     // vector to fill the BIC computation for each simulated draw generated from model j but assuming here that model m is true
                     arma::vec fillVec = vec(noSimDraws);
-                    for(int d=0;d<noSimDraws;d++) // indexes data generated from a specific regression fit
-                    {
-                        arma::vec tempYs = tempYMat.col(d); // pull a particular simulated sample
-                        fillVec[d] = normBICComp(tempYs,curModel); // compute the BIC for this fit
-                    }
+                    fillVec=normBICComp2(tempYMat,curModel,fillVec,noSimDraws);
                     fillMat.col(m) = fillVec; // column m of fillMat will hold the BICs for each draw computed under model m
                 }
                 returnListSub2[k] = fillMat; // store all BIC computations for normal fit k
@@ -146,11 +204,7 @@ List RcppSimDGOFs(List yList,Rcpp::StringVector models)
                     String curModel = models[m];
                     // vector to fill the BIC computation for each simulated draw generated from model j but assuming here that model m is true
                     arma::vec fillVec = vec(noSimDraws);
-                    for(int d=0;d<noSimDraws;d++) // indexes data generated from a specific regression fit
-                    {
-                        arma::vec tempYs = tempYMat.col(d); // pull a particular simulated sample
-                        fillVec[d] = normBICComp(tempYs,curModel); // compute the BIC for this fit
-                    }
+                    fillVec=normBICComp2(tempYMat,curModel,fillVec,noSimDraws);
                     fillMat.col(m) = fillVec; // column m of fillMat will hold the BICs for each draw computed under model m
                 }
                 arma::mat tempDGOFs = DGOFCompforMatrix(fillMat);
